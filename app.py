4e6ce419e038c36a8162b90fb8357ff9c3374882
RyoPCK <rryyoobb@gmail.com>
2024-06-04 21:18:29 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import yt_dlp
+import os
+
+app = Flask(__name__)
+
+@app.route('/download', methods=['POST'])
+def download():
+    data = request.json
+    url = data['url']
+
+    ydl_opts = {
+        'outtmpl': 'downloaded_video.%(ext)s',
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=True)
+            video_ext = info_dict.get('ext', 'mp4')
+            video_file = f'downloaded_video.{video_ext}'
+        
+        return jsonify({'status': 'success', 'filePath': video_file})
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
